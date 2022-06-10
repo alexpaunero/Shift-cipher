0d0e1fb6e775812752a19c14cd8f07ca8e936ae0
alexpaunero <103605819+alexpaunero@users.noreply.github.com>
2022-06-10 22:27:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ class ShiftCipher {
     for (let i = 0; i < text.length; i++){
       let charCode = text.charCodeAt(i);
       if (charCode >= 65 && charCode <= 90) {
-        if (charCode + this.shift > 90){
+        if (charCode + this._shift > 90){
           let newCharCode = 64 + charCode + this._shift - 90;
           encrypted.push(String.fromCharCode(newCharCode));
         } else {
@@ -21,12 +21,30 @@ class ShiftCipher {
       }
     }
     return encrypted.join('');
-  }
-  decrypt(text) {
-
+  } decrypt(text) {
+    text = text.toLowerCase();
+    let decrypted = [];
+    for (let i = 0; i < text.length; i++){
+      let charCode = text.charCodeAt(i);
+      if (charCode >= 97 && charCode <= 122) {
+        if (charCode - this._shift < 97){
+          let newCharCode = charCode - this._shift + (122 - 96);
+          decrypted.push(String.fromCharCode(newCharCode));
+        } else {
+          let newCharCode = charCode - this._shift;
+          decrypted.push(String.fromCharCode(newCharCode));
+        }
+      } else {
+          let newCharCode = charCode;
+          decrypted.push(String.fromCharCode(newCharCode));
+      }
+    }
+    return decrypted.join('');
   }
 
 }
 
 const cipher = new ShiftCipher(2);
 console.log(cipher.encrypt('I love to code!'));
+console.log(cipher.decrypt('K <3 OA RWRRA'));
+console.log(cipher.decrypt('a'));
